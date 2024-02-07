0cfd58eb27d7cb85994030945d8ca175fde8d4bb
daulat <daulatjajra2006@gmail.com>
2024-02-07 14:12:43 +0530

    winner

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 "use strict ";
 
-let score0El = document.getElementById("score0");
+let score0El = document.querySelector("#score0");
 let score1El = document.querySelector("#score1");
-let player0El = document.getElementById("player0");
-let player1El = document.querySelector("#player1");
-let current0El = document.getElementById("current0");
+let current0El = document.querySelector("#current0");
 let current1El = document.querySelector("#current1");
-let diceEl = document.querySelector(".dice ");
+let player0El = document.querySelector(".player0");
+let player1El = document.querySelector(".player1");
+let diceEl = document.querySelector(".dice");
+
 let btnNew = document.querySelector(".btnNew");
 let btnRoll = document.querySelector(".btnRoll");
 let btnHold = document.querySelector(".btnHold");
 
-let scores, currentScore, activePlayer, playing;
+let score, currentScore, activePlayer, playing;
 
+// Initialize
 let init = function () {
-  scores = [0, 0];
+  score = [0, 0];
   currentScore = 0;
   activePlayer = 0;
   playing = true;
 
+  // hide the dice
   diceEl.classList.add("hidden");
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+  // set all scores to 0
+  score0El.textContent =
+    score1El.textContent =
+    current0El.textContent =
+    current1El.textContent =
+      0;
 
+  // Remove the winner class from both players , add active class to player 0 and remove from player 1
   player0El.classList.remove("playerWinner");
   player1El.classList.remove("playerWinner");
   player0El.classList.add("playerActive");
@@ -41,13 +47,11 @@ let switchPlayer = () => {
   player1El.classList.toggle("playerActive");
 };
 
-btnRoll.addEventListener("click", () => {
+let roll = function () {
   if (playing) {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    console.log(dice);
+    let dice = +Math.floor(Math.random() * 6) + 1;
     diceEl.classList.remove("hidden");
     diceEl.src = `Imgs/${dice}.png`;
-
     if (dice !== 1) {
       currentScore += dice;
       document.getElementById(`current${activePlayer}`).textContent =
@@ -56,14 +60,14 @@ btnRoll.addEventListener("click", () => {
       switchPlayer();
     }
   }
-});
+};
 
-btnHold.addEventListener("click", () => {
+let hold = function () {
   if (playing) {
-    scores[activePlayer] += currentScore;
+    score[activePlayer] += currentScore;
     document.getElementById(`score${activePlayer}`).textContent =
-      scores[activePlayer];
-    if (scores[activePlayer] >= 100) {
+      score[activePlayer];
+    if (score[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player${activePlayer}`)
@@ -76,6 +80,7 @@ btnHold.addEventListener("click", () => {
       switchPlayer();
     }
   }
-});
-
+};
+btnHold.addEventListener("click", hold);
+btnRoll.addEventListener("click", roll);
 btnNew.addEventListener("click", init);
